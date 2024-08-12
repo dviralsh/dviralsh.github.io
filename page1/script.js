fa21083d47b6d548cb7e07b63c81de152c502845
dviralsh <dviralsh@gmail.com>
2024-08-12 11:24:01 +0300

    Creating new page

--- page1/script.js
+++ page1/script.js
@@ -1,5 +1,4 @@
 document.getElementById('myButton').addEventListener('click', function() {
     alert('Button clicked!');
-	window.location.replace("https://github/dviralsh/dviralsh.github.io/page1/index.html");
 });
 
